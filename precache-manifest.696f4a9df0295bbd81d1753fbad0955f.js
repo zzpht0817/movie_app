c239293692a675f9511e41b40b9ef8287016c1e4
zzpht <zzpht001@gmail.com>
2019-03-22 19:04:07 +0900

    Updates

---
+++ precache-manifest.696f4a9df0295bbd81d1753fbad0955f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "ad946fa40fe6905d68c4",
+    "url": "/movie_app/static/js/main.6f4bd89d.chunk.js"
+  },
+  {
+    "revision": "df0b9ca0f81249544de6",
+    "url": "/movie_app/static/js/2.1267fdac.chunk.js"
+  },
+  {
+    "revision": "ad946fa40fe6905d68c4",
+    "url": "/movie_app/static/css/main.e45ee24a.chunk.css"
+  },
+  {
+    "revision": "3cd2a0980155bbea1d74d42d468d4796",
+    "url": "/movie_app/index.html"
+  }
+];
